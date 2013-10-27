780b893725408bea230cd85c28ce9303d2e547cb
cloud <sanllanta@gmail.com>
2013-10-27 14:44:24 -0500

    amazonElasticache

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 require 'action_dispatch/middleware/session/dalli_store'
-Rails.application.config.session_store :dalli_store, :memcache_server => ['localhost:11211'], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 20.minutes
+Rails.application.config.session_store :dalli_store, :memcache_server => ['cloudsession.n9rvzs.cfg.use1.cache.amazonaws.com:11211'], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 20.minutes
