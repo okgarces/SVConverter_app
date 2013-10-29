cc0b9084e943bdf0ed26fdc7a93476e8c4ce3dfd
okgarces <ok.garces10@uniandes.edu.co>
2013-10-29 10:53:07 -0500

    Agregar configuracion a development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ SVConverterApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  require 'active_support/cache/dalli_store'
+  config.cache_store = :dalli_store
 end
