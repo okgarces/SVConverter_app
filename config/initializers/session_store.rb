95215e6b966bf9dd031b9ef5564013468ccc667a
okgarces <ok.garces10@uniandes.edu.co>
2013-10-29 18:50:49 -0500

    corrección del error para iniciar sesion y uso de gem dalli 2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,5 @@
 
 #SVConverterApp::Application.config.session_store :cookie_store, key: '_SVConverter_app_session'
 
-# require 'action_dispatch/middleware/session/dalli_store'
+require 'action_dispatch/middleware/session/dalli_store'
 Rails.application.config.session_store :dalli_store, :memcache_server => ['sessionconverter.n9rvzs.cfg.use1.cache.amazonaws.com:11211'], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 20.minutes
