e23bd2406ab819d85564f82736638219cc1524e9
okgarces <ok.garces10@uniandes.edu.co>
2013-11-18 15:00:42 -0500

    Corrección FFMPEG

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module SVConverterApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile += %w(*.png *.jpg *.gif *.jpeg)
+    config.assets.precompile += %w(*.png *.jpg *.gif *.jpeg jwplayer.js)
   end
 end
